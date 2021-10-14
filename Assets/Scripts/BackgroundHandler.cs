61fb5cdc520c19a8cfabcf80d635da944340826f
UnOso <manuelluis736@gmail.com>
2021-10-14 12:20:33 +0200

    Map Generates as you go

--- Assets/Scripts/BackgroundHandler.cs
+++ Assets/Scripts/BackgroundHandler.cs
@@ -6,26 +6,26 @@ public class BackgroundHandler : MonoBehaviour
 {
     public Transform[] spawnPos = new Transform[2];
     public GameObject[] bg = new GameObject[1];
-    public float distanceToSpawnBg = 3.0f;
-    private float distanceTravelled;
+    public float distanceToSpawnBg = 4.5f;
+    private float distanceTravelled = 0f;
+    private float offset = 3.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        distanceTravelled = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (transform.position.y >= distanceTravelled + distanceToSpawnBg)
-            SpawnPlatform(transform.position.y);
+            SpawnBg();
     }
 
-    private void SpawnPlatform(float yPos)
+    private void SpawnBg()
     {
-        Instantiate(bg[0], new Vector2(spawnPos[0].position.x, spawnPos[0].position.y), Quaternion.identity);
-        Instantiate(bg[1], new Vector2(spawnPos[1].position.x, spawnPos[1].position.y), Quaternion.identity);
-        distanceTravelled = transform.position.y;
+        Instantiate(bg[0], new Vector2(spawnPos[0].position.x, spawnPos[0].position.y + offset), Quaternion.identity);
+        Instantiate(bg[1], new Vector2(spawnPos[1].position.x, spawnPos[1].position.y + offset), Quaternion.identity);
+        distanceTravelled = Mathf.Round(transform.position.y * 10)/10;
     }
 }
